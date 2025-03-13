6edac826e4215421dc03677c85eab6f54c8ec4b2
Bart Venter <bartventer@outlook.com>
2025-03-13 12:15:41 +0000

    docs(playground): Update package level documentation

--- cmd/playground/main.go
+++ cmd/playground/main.go
@@ -1,11 +1,11 @@
 //go:build js && wasm
 // +build js,wasm
 
-package main
-
 // Playground is a WebAssembly module that provides functions for processing Go templates.
 //
 // See [playground] for information.
+package main
+
 import (
 	"github.com/bartventer/go-template-playground/internal/playground"
 )
